dfc895368449736ab7a14b80084a0728ab5b74de
jon-r <jonr.web@gmail.com>
2015-11-12 11:22:09 +0000

    first class test

---
+++ functions/classes/JR_class_compile.php
@@ -0,0 +1,158 @@
+<?php
+class compile {
+
+/*
+private $refNum;
+  private $rawArr;
+  private $detail;
+  private $ss;
+  */
+
+/*  public function setArr($arr) {
+    $this->rawArr = $arr;
+  }*/
+
+  private function MMtoFeet() {
+    $justInches = $this / 25.4;
+    if ($justInches < 24) {
+      $out = ceil($justInches).'in';
+    } else {
+      $feet = floor($justInches / 12);
+      $inches = $justInches % 12;
+      $out = $feet."ft ";
+      $out .= $inches > 0 ? $inches.'in' : null;
+    }
+    return $out;
+  }
+
+  private function setBrand() {
+    if ($this->Brand) {
+      $brandUrl = sanitize_title($this->Brand);
+      $brandImg = jr_siteImg('brands/long/'.$brandUrl.'-logo.jpg', $relative = true);
+      if (file_exists(ABSPATH.$brandImg)) {
+        $brandText = '<img class="framed" src="'.site_url($brandImg).'" alt="'.$this->Brand.'" >'
+          .'<a href="'.home_url('products/brand/'.$brandUrl).'" >More from '.$this->Brand.'</a>';
+      } else {
+        $brandText = $this->Brand.' (<a href="'.home_url('products/brand/'.$brandUrl).'" >More</a>)';
+      }
+    } else {
+      $brandText = null;
+    };
+    return $brandText;
+  }
+
+  private function setPower() {
+    if ($this->Wattage >= 1500) {
+      $pwrCheck = ($this->Wattage / 1000).'kw, '.$this->Power;
+    } elseif ($this->Wattage < 1500 && $this->Wattage > 0) {
+      $pwrCheck = $this->Wattage.' watts, '.$this->Power;
+    } elseif ($this->Power) {
+      $pwrCheck = $this->Power;
+    } else {
+      $pwrCheck = null;
+    };
+    return $pwrCheck;
+  }
+
+  private function setSpecs() {
+    $out = [
+      'Brand'   => $this->setBrand() ?:null,
+      'Model'   => $this->Model ?: null,
+      'Power'   => $this->setPower() ?: null,
+      'Height'  => $this->Height ? $this->Height.'mm / '.MMtoFeet($this->Height) : null,
+      'Width'   => $this->Width ? $this->Width.'mm / '.MMtoFeet($this->Width) : null,
+      'Depth'   => $this->Depth ? $this->Depth.'mm / '.MMtoFeet($this->Depth) : null,
+    ];
+    if ($this->ExtraMeasurements) {
+      $extras = explode(';',$this->ExtraMeasurements);
+      foreach($extras as $extra) {
+        if (strpos($extra, ":") === false) {
+          $out[] = trim($extra);
+        } else {
+          $item = explode(':',$extra);
+          $out[trim($item[0])] = trim($item[1]);
+        }
+      }
+    };
+    //put last to keep in order
+    $out['Please Note'] = $this->{'Condition/Damages'} != "0" ? $this->{'Condition/Damages'} : null;
+    return array_filter($out);
+  }
+
+  private function setImgs() {
+    $name = ($this->ss) ? 'RHCs' : 'RHC';
+    $out = glob('rhc/images/gallery/'.$name.$this->refNum.'[!0-9]*', GLOB_BRACE);
+    return $out;
+  }
+
+  private function setIcon() {
+    if ($this->ss) {
+      $catArray = [$this->Category;
+    } else {
+      $catArray = [$this->Category,  $this->Cat1,  $this->Cat2, $this->Cat3 ];
+    }
+
+    if (in_array('Fridges', $catArray) && in_array('Freezers', $catArray)) {
+      $iconCheck = 'fridge-freezer';
+    } elseif (in_array('Fridges', $catArray)) {
+      $iconCheck = 'fridge';
+    } elseif (in_array('Freezers', $catArray)) {
+      $iconCheck = 'freezer';
+    } elseif ($this->Power) {
+      $iconCheck = str_replace(' ', '-', strtolower($this->Power));
+    } else {
+      $iconCheck = null;
+    };
+    return $iconCheck;
+  }
+
+  private function setInfo() {
+    if (isset($this->isSale)) {
+      $infoCheck = "sale";
+    } elseif ($this->Quantity == 0) {
+      $infoCheck = "sold";
+    } elseif (in_array($this->RHC, $newCheck)) {
+      $infoCheck = "new";
+    } else {
+      $infoCheck = null;
+    };
+    return $infoCheck;
+  }
+
+  public function compile($dbRaw,$detail) {
+
+    $out = array();
+
+    switch ($detail) {
+      case ('Full') :
+        array_push($out, [
+          'height'    => $dbRaw->Height ?: null,
+          'width'     => $dbRaw->Width ?: null,
+          'depth'     => $dbRaw->Depth ?: null,
+          'desc'      => ($dbRaw->{'Line 1'} != "0" ? '<p>'.$dbRaw->{'Line 1'}.'</p>' : null).
+                         ($dbRaw->{'Line 2'} != "0" ? '<p>'.$dbRaw->{'Line 2'}.'</p>' : null).
+                         ($dbRaw->{'Line 3'} != "0" ? '<p>'.$dbRaw->{'Line 3'}.'</p>' : null),
+          'specs'     => $dbRaw->setSpecs(),
+          'imgAll'    => $dbRaw->setImgs(),
+          //this glob targets only the valid RHC reference. ie 'RHC10', 'RHC10 b', NOT 'RHC101'
+          'category'  => $dbRaw->'Category'
+        ]);
+      case ('Tile') :
+        array_push($out, [
+          'widthFt'  => $dbRaw->Width ? MMtoFeet($dbRaw->Width) : null,
+          'icon'     => $dbRaw->setIcon(),
+          'info'     => $dbRaw->setInfo(),
+          'quantity' => $dbRaw->Quantity > 1 ? $dbRaw->Quantity." in Stock" : null,
+        ]);
+      case ('Lite') :
+        array_push($out, [
+          'price'    => $priceCheck,
+          'webLink'  => 'rhc/'.$dbRaw->RHC.'/'.sanitize_title($dbRaw->ProductName),
+          'rhc'      => 'ref: RHC'.$dbRaw->RHC,
+          'name'     => $dbRaw->ProductName,
+          'imgFirst' => jr_siteImg('gallery/RHC'.$dbRaw->RHC.'.jpg'),
+        ]);
+    }
+    return $out;
+  }
+  ?>
--- functions/classes/JR_class_product.php
+++ functions/classes/JR_class_product.php
@@ -4,9 +4,22 @@ class productSingle {
 
   private $refNum;
   private $table;
+  private $err;
 
   private $dbRaw = array();
   private $ss = strtolower($table) == 'rhcs';
+
+  public function setRef($rhcRef) {
+    preg_match('/(RHCs?)(\d+)/i',$rhcRef, $refArray);
+//    $ref = preg_split('/(RHCs?)(\d+)/i', $rhcRef, PREG_SPLIT_DELIM_CAPTURE);
+    if ($refArray !== false) {
+      $this->refNum = $refArray[0];
+      $this->table = $refArray[1];
+    } else {
+      $this->err = 'Invalid Ref: '.$rhcRef;
+    }
+
+  }
 
   private function validate() {
     global $wpdb, $itemSoldDuration;
@@ -24,8 +37,25 @@ class productSingle {
 
   private $isValid = $this->validate();
 
-  private function getDbInfo() {
-    return $this->dbRaw = jrQ_getItem($table,$safeRHC);
+  private function setDbInfo() {
+    if ($this->err == '') {
+      if ($this->isValid) {
+        $this->dbRaw = jrQ_getItem($table,$rhcRef);
+      } else {
+        $this->err = 'Not Found'
+      }
+    }
+  }
+
+  public function getCompiled($detail) {
+    if ($this->err == '') {
+      $array = $this->setDbInfo();
+      $item = new compile;
+      $out = $item->complile($array,$detail);
+    } else {
+      $out['err'] = $this->err;
+    }
+    return $out;
   }
 
 }
@@ -40,145 +70,4 @@ class productMulti {
 }
 */
 
-class compile {
 
-  private $refNum;
-  private $detail;
-  private $ss;
-
-  private function MMtoFeet($mm) {
-    $justInches = $mm / 25.4;
-    if ($justInches < 24) {
-      $out = ceil($justInches).'in';
-    } else {
-      $feet = floor($justInches / 12);
-      $inches = $justInches % 12;
-      $out = $feet."ft ";
-      $out .= $inches > 0 ? $inches.'in' : null;
-    }
-    return $out;
-  }
-
-  private function getBrand() {
-    if ($this->Brand) {
-      $brandUrl = sanitize_title($this->Brand);
-      $brandImg = jr_siteImg('brands/long/'.$brandUrl.'-logo.jpg', $relative = true);
-      if (file_exists(ABSPATH.$brandImg)) {
-        $brandText = '<img class="framed" src="'.site_url($brandImg).'" alt="'.$this->Brand.'" >'
-          .'<a href="'.home_url('products/brand/'.$brandUrl).'" >More from '.$this->Brand.'</a>';
-      } else {
-        $brandText = $this->Brand.' (<a href="'.home_url('products/brand/'.$brandUrl).'" >More</a>)';
-      }
-    } else {
-      $brandText = null;
-    };
-    return $brandText;
-  }
-
-  private function getPower() {
-    if ($this->Wattage >= 1500) {
-      $pwrCheck = ($this->Wattage / 1000).'kw, '.$this->Power;
-    } elseif ($this->Wattage < 1500 && $this->Wattage > 0) {
-      $pwrCheck = $this->Wattage.' watts, '.$this->Power;
-    } elseif ($this->Power) {
-      $pwrCheck = $this->Power;
-    } else {
-      $pwrCheck = null;
-    };
-    return $pwrCheck;
-  }
-
-  private function getSpecs() {
-    $out = [
-      'Brand'   => $this->getBrand() ?:null,
-      'Model'   => $this->Model ?: null,
-      'Power'   => $this->getPower() ?: null,
-      'Height'  => $this->Height ? $this->Height.'mm / '.MMtoFeet($this->Height) : null,
-      'Width'   => $this->Width ? $this->Width.'mm / '.MMtoFeet($this->Width) : null,
-      'Depth'   => $this->Depth ? $this->Depth.'mm / '.MMtoFeet($this->Depth) : null,
-    ];
-    if ($this->ExtraMeasurements) {
-      $extras = explode(';',$this->ExtraMeasurements);
-      foreach($extras as $extra) {
-        if (strpos($extra, ":") === false) {
-          $out[] = trim($extra);
-        } else {
-          $item = explode(':',$extra);
-          $out[trim($item[0])] = trim($item[1]);
-        }
-      }
-    };
-    //put last to keep in order
-    $out['Please Note'] = $this->{'Condition/Damages'} != "0" ? $this->{'Condition/Damages'} : null;
-    return array_filter($out);
-  }
-
-  private function getImgs() {
-    $name = ($this->ss) ? 'RHCs' : 'RHC';
-    $out = glob('rhc/images/gallery/'.$name.$this->refNum.'[!0-9]*', GLOB_BRACE);
-    return $out;
-  }
-
-  private function getIcon() {
-    if ($this->ss) {
-      $catArray = [$ref['Category']];
-    } else {
-      $catArray = [ $ref['Category'],  $ref['Cat1'],  $ref['Cat2'], $ref['Cat3'] ];
-    }
-
-    if (in_array('Fridges', $catArray) && in_array('Freezers', $catArray)) {
-      $iconCheck = 'fridge-freezer';
-    } elseif (in_array('Fridges', $catArray)) {
-      $iconCheck = 'fridge';
-    } elseif (in_array('Freezers', $catArray)) {
-      $iconCheck = 'freezer';
-    } elseif ($ref['Power']) {
-      $iconCheck = str_replace(' ', '-', strtolower($ref['Power']));
-    } else {
-      $iconCheck = null;
-    };
-    return $iconCheck;
-  }
-
-  private function getInfo() {
-    if (isset($ref['isSale'])) {
-      $infoCheck = "sale";
-    } elseif ($ref['Quantity'] == 0) {
-      $infoCheck = "sold";
-    } elseif (in_array($ref['RHC'], $newCheck)) {
-      $infoCheck = "new";
-    } else {
-      $infoCheck = null;
-    };
-    return $infoCheck;
-  }
-
-  public function compile($dbRaw,$detail) {
-
-    switch ($detail) {
-      case ('Full') :
-        $out1 = [
-          'height'    => $dbRaw->Height ?: null,
-          'width'     => $dbRaw->Width ?: null,
-          'depth'     => $dbRaw->Depth ?: null,
-          'desc'      => ($dbRaw->{'Line 1'} != "0" ? '<p>'.$dbRaw->{'Line 1'}.'</p>' : null).
-                         ($dbRaw->{'Line 2'} != "0" ? '<p>'.$dbRaw->{'Line 2'}.'</p>' : null).
-                         ($dbRaw->{'Line 3'} != "0" ? '<p>'.$dbRaw->{'Line 3'}.'</p>' : null),
-          'specs'     => $dbRaw->getSpecs(),
-          'imgAll'    => $dbRaw->getImgs(),
-          //this glob targets only the valid RHC reference. ie 'RHC10', 'RHC10 b', NOT 'RHC101'
-          'category'  => $dbRaw->'Category'
-        ];
-      case ('Tile') :
-        $out2 = [
-          'icon'     => $dbRaw->iconCheck(),
-          'info'     => $dbRaw->infoCheck(),
-          'quantity' => $ref['Quantity'] > 1 ? $ref['Quantity']." in Stock" : null,
-        ];
-      case ('Lite') :
-        $out3 = [
-
-        ]
-    }
-
-  }
