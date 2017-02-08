554dea2cacbb588fb13aa05bfd4f05e6d38362d1
Jon <jonr.web@gmail.com>
2017-02-08 22:00:36 +0000

    added features to carousel

--- functions/JR_Carousel.php
+++ functions/JR_Carousel.php
@@ -10,24 +10,44 @@
 function jr_magicRoundabout($slideIn) {
 
   $out = [
-    'title'   => $slideIn['Title'],
-    'titleCol'=> jr_styleCarousel($slideIn['TitleColour']),
-    'titlePos'=> jr_positionCarousel($slideIn['TitlePos']),
-    'text1'   => $slideIn['Description'] != "0" ? $slideIn['Description'] : null,
-    'text2'   => $slideIn['Desc2'] != "0" ? $slideIn['Desc2'] : null,
-    'text3'   => $slideIn['Desc3'] != "0" ? $slideIn['Desc3'] : null,
-    'textPos' => jr_positionCarousel($slideIn['TextPos']),
-    'style1'  => jr_styleCarousel($slideIn['Desc1Emphasis']),
-    'style2'  => jr_styleCarousel($slideIn['Desc2Emphasis']),
-    'style3'  => jr_styleCarousel($slideIn['Desc3Emphasis']),
-    'image'   => jr_siteImg('gallery/'.$slideIn['ImageRef'].'.jpg'),
-    'link'    => $slideIn['WebLink'] != "0" ? $slideIn['WebLink'] : null,
-    'linkPos' => jr_positionCarousel($slideIn['ClickHerePos']),
-    'linkCol' => jr_styleCarousel($slideIn['ClickHereColour'])
+    'title'       => $slideIn['Title'],
+    'titlePos'    => jr_positionCarousel($slideIn['TitlePos']),
+    'titleColor'  => jr_colorCarousel($slideIn['title_color']),
+    'titleShow'   => $slideIn['title_show'],
+    'titleBold'   => $slideIn['title_bold'] ? 'go-bold' : null,
+
+    'text1'       => $slideIn['Description'] != "0" ? $slideIn['Description'] : null,
+    'text1Pos'    => null,
+    'text1Color'  => jr_colorCarousel($slideIn['desc1_color']),
+    'text1Show'   => $slideIn['desc1_show'],
+    'text1Bold'   => $slideIn['desc1_bold'] ? 'go-bold' : null,
+
+    'text2'       => $slideIn['Desc2'] != "0" ? $slideIn['Desc2'] : null,
+    'text2Pos'    => null,
+    'text2Color'  => jr_colorCarousel($slideIn['desc2_color']),
+    'text2Show'   => $slideIn['desc2_show'],
+    'text2Bold'   => $slideIn['desc2_bold'] ? 'go-bold' : null,
+
+    'text3'       => $slideIn['Desc3'] != "0" ? $slideIn['Desc3'] : null,
+    'text3Pos'    => null,
+    'text3Color'  => jr_colorCarousel($slideIn['desc3_color']),
+    'text3Show'   => $slideIn['desc3_show'],
+    'text3Bold'   => $slideIn['desc3_bold'] ? 'go-bold' : null,
+
+    'link'        => 'Click Here',
+    'linkPos'     => jr_positionCarousel($slideIn['ClickHerePos']),
+    'linkColor'   => jr_colorCarousel($slideIn['link_color']),
+    'linkShow'    => $slideIn['link_show'],
+    'linkBold'    => $slideIn['link_bold'] ? 'go-bold' : null,
+
+    'weblink'        => $slideIn['WebLink'] != "0" ? $slideIn['WebLink'] : null,
+    'textPos'     => jr_positionCarousel($slideIn['TextPos']),
+    'image'       => jr_siteImg('gallery/'.$slideIn['ImageRef'].'.jpg'),
   ];
 
   return $out;
 }
+
 
 /**
  * sets the position class of the carousel element
@@ -48,26 +68,36 @@ function jr_positionCarousel($in) {
 }
 
 /**
- * sets the text style of the carousel element
+ * validates the text color of the carousel element
  * @param  string $in style value from table
- * @return string css class result
+ * @return string hex color result
  */
-function jr_styleCarousel($in) {
+function jr_colorCarousel($in) {
 
-  if ($in == "Bold") {
-    $out = "go-bold";
-    } elseif ($in == "White") {
-    $out = "go-white";
-  } elseif ($in == "Bold_White") {
-    $out = "go-bold go-white";
-  } elseif ($in == "Red") {
-    $out = "go-red";
-  } elseif ($in == "Bold_Red") {
-    $out = "go-bold go-red";
+  if (ctype_xdigit($in) && strlen($in) == 6) {
+    $out = $in;
+  } else {
+    $out = "000";
+  }
+
+  return $out;
+}
+
+
+function jr_carouselStr($slide, $target, $el, $className) {
+  if ($slide["{$target}Show"]) {
+    $strFormat = '<%1$s class="%2$s" style="color:#%3$s" >%4$s</%1$s>';
+    $class = join(' ', [$className, $slide["{$target}Bold"], $slide["{$target}Pos"]]);
+    $color = $slide["{$target}Color"];
+    $value = $slide[$target];
+
+    $out = sprintf($strFormat, $el, $class, $color, $value);
+
   } else {
     $out = null;
   }
 
   return $out;
 }
+
 ?>
--- includes/carousel.php
+++ includes/carousel.php
@@ -13,17 +13,17 @@ $carouselCount = count($carouselList);
       ?>
 
       <li carousel-slide class="slide<?php echo $i == 0 ? ' is-active' : null ?>" >
-        <a href="<?php echo $slide['link']; ?>">
+        <a href="<?php echo $slide['weblink']; ?>">
           <img class="framed" src="<?php echo $slide['image']; ?>" alt="<?php echo $slide['title']; ?>" >
-          <h2 class="slider-title <?php echo $slide['titlePos'].' '.$slide['titleCol']; ?>"><?php echo $slide['title']; ?></h2>
+          <?php echo jr_carouselStr($slide, 'title', 'h2', 'slider-title'); ?>
 
           <div class="slider-text <?php echo $slide['textPos']; ?>">
-            <span class="<?php echo $slide['style1']; ?>"><?php echo $slide['text1']; ?></span>
-            <span class="<?php echo $slide['style2']; ?>"><?php echo $slide['text2']; ?></span>
-            <span class="<?php echo $slide['style3']; ?>"><?php echo $slide['text3']; ?></span>
+            <?php echo jr_carouselStr($slide, 'text1', 'span', null); ?>
+            <?php echo jr_carouselStr($slide, 'text2', 'span', null); ?>
+            <?php echo jr_carouselStr($slide, 'text3', 'span', null); ?>
           </div>
 
-          <h3 class="slider-link <?php echo $slide['linkPos'].' '.$slide['linkCol']; ?>">Click Here</h3>
+          <?php echo jr_carouselStr($slide, 'link', 'h3', 'slider-link'); ?>
         </a>
       </li>
 
